3825f6a254b7f6302697e7e078d57c3ed98cb422
ezirmusitua <jferroal@gmail.com>
2018-08-15 09:31:40 +0800

    Feat: add convert logic

--- csv2xlsx/views.py
+++ csv2xlsx/views.py
@@ -1,4 +1,9 @@
-from flask import render_template, request
+from csv import DictReader
+from io import StringIO
+from tempfile import TemporaryFile
+
+from flask import render_template, request, send_file
+from xlwt import Workbook
 
 
 def hello():
@@ -10,6 +15,18 @@ def index():
 
 
 def handle_and_download():
-    print(request.form)
-    print(request.files)
-    return 'Handled'
+    wb = Workbook()
+    for fileName in request.files:
+        print(fileName)
+        ws = wb.add_sheet(fileName)
+        reader = DictReader(StringIO(request.files[fileName].read().decode()))
+        for c, val in enumerate(reader.fieldnames):
+            ws.write(0, c, val)
+        for r, row in enumerate(reader, 1):
+            for c, field in enumerate(row):
+                ws.write(r, c, row[field])
+    # try in memory fp with string io
+    fp = TemporaryFile()
+    wb.save(fp)
+    fp.seek(0)
+    return send_file(fp, as_attachment=True, attachment_filename='demo.xlsx')
